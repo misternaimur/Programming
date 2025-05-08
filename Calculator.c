e35382d43e6e677982ab44d8fb9c89f04e9b50f8
Md Naimur Rahman <jawadnaimur@gmail.com>
2025-05-08 18:40:07 +0600

    first commit

--- Calculator.c
+++ Calculator.c
@@ -1,7 +1,47 @@
- // Create mini Calculator 
+// Create mini Calculator 
 
- #include <stdio.h>
- int main ()
- {
-     int 
- }
+#include <stdio.h>
+
+int main()
+{
+    int a, b, Result;
+    char icon;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &icon);
+
+    if (icon == '+')
+    {
+        Result = a + b;
+    }
+    else if (icon == '-')
+    {
+        Result = a - b;
+    }
+    else if (icon == '/')
+    {
+        if (b == 0)
+        {
+            printf("Error: Division by zero is not allowed.\n");
+        }
+        Result = a / b;
+    }
+    else if (icon == '*')
+    {
+        Result = a * b;
+    }
+    else
+    {
+        printf("You selected a wrong character.\n");
+    }
+
+    printf("The Result is: %d\n", Result); 
+
+    return 0;
+}

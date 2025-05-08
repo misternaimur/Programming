1f27c67af9c3d845b83c7237ba9023fd7c9454e0
Md Naimur Rahman <jawadnaimur@gmail.com>
2025-05-08 19:10:56 +0600

    first commit

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,15 @@
+// Simple Interest (SI) = (P × R × T) / 100
+#include <stdio.h>
+int main()
+{
+     float  Principal , Rate , Time , SI ; 
+
+     printf("Principal :");
+     scanf("%f",&Principal);
+     printf("Rate :");
+     scanf("%f",&Rate);
+     printf("Time : ");
+     scanf("%f",&Time);
+     SI = (Principal*Rate*Time)/100;
+     printf("%.2f",SI);
+     return 0 ;

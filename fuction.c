e9a713404071a3ecf8ebbb2d92e9e06171616f9d
Md Naimur Rahman <jawadnaimur@gmail.com>
2025-06-12 20:42:25 +0600

    problem Solving

---
+++ fuction.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+// declaration / prototype 
+void printhello();
+
+int main()
+
+{
+    printhello(); // function call 
+     return 0;
+}
+
+// function definition 
+void printhello()
+{
+   printf("Hello world");
+}

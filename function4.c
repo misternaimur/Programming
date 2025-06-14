fc4949cb9113b7e56ff537fdba33c922a18a3fd3
Md Naimur Rahman <jawadnaimur@gmail.com>
2025-06-14 20:23:52 +0600

    problem Solving

---
+++ function4.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+float squarearea( float side );
+float circlearea( float rad );
+float Rectangle( float a , float b );
+int main ()
+{
+  float a = 5.9;
+  float b = 10.0 ; 
+
+  printf("area is : %f", Rectangle(a,b) );
+  return 0 ;
+ }
+
+ float squarearea(float side ){
+  return side * side ;
+
+ }
+
+ float circlearea(float rad ){
+
+  return 3.14 * rad * rad ;
+ }
+
+ float Rectangle( float a , float b )
+ {
+  return a * b ;
+ }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,15 +1,4 @@
-// Simple Interest (SI) = (P × R × T) / 100
-#include <stdio.h>
-int main()
-{
-     float  Principal , Rate , Time , SI ; 
-
-     printf("Principal :");
-     scanf("%f",&Principal);
-     printf("Rate :");
-     scanf("%f",&Rate);
-     printf("Time : ");
-     scanf("%f",&Time);
-     SI = (Principal*Rate*Time)/100;
-     printf("%.2f",SI);
-     return 0 ;
+/**
+ * Calculates a final price by adding an 18% tax.
+ * @param base_price The initial price/value.
+ * @return The price after tax.

28d41ce5227666daf0f47d50c812757a59ba1622
Md Naimur Rahman <jawadnaimur@gmail.com>
2025-06-18 16:04:48 +0600

    problem Solving

--- facetorial.c
+++ facetorial.c
@@ -12,7 +12,7 @@ int factorial(int n) {
     if (n == 0) {
         return 1;
     }
-    int factNm1 = factorial(n - 1);
+    int factNm1 =  factorial(n - 1);
     int factN = factNm1 * n;
     return factN;
 }
